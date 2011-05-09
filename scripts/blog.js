b2b9873489402e16bb2330b1ae2b77ae0b3247f0
James Edward Gray II <james@graysoftinc.com>
2011-05-09 15:37:21 -0500

    Adding the nine month shots.

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 	$('a.photo').colorbox({
-		initialWidth:  "640px",
-		initialHeight: "480px",
-		innerWidth:    "640px",
-		innerHeight:   "480px",
-		maxWidth:      "640px",
-		maxHeight:     "480px",
-		scalePhotos:   false,
-		scrolling:     false,
-		photo:         true
+		innerWidth:  "640px",
+		innerHeight: "480px"
 	});
 });
